1b76dd4678d8c5e6a6d061622910af2d185a4960
briannaordaz <briannaordaz@gmail.com>
2024-11-08 17:13:45 -0800

    finished numbers 1 and 2

--- assignment5/script.js
+++ assignment5/script.js
@@ -2,12 +2,30 @@
 // containing all the numbers from start up to (and including) end.
 // Example output:
 // console.log(range(1, 10)); // outputs [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+function idk (start, end) {
+    const outcome = [];
+    for (let i = start; i <= end; i++) {
+        outcome.push(i);
+    }
+    return outcome;
+}
+
+console.log(idk(50, 60)); // output: (11) [50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60]
 
 
 // 2. Next, write a sum function that takes an array of numbers and returns the sum of
 // these numbers. Run the example program and see whether it does indeed return 55.
 // Example output:
 // console.log(sum(range(1, 10))); // returns 55
+function all(numbers) {
+    let amount = 0;
+    for (let digits of numbers) {
+        amount += digits;
+    }
+    return amount;
+}
+
+console.log(all(idk(1, 10))); // output: 55
 
 
 // 3. Arrays have a reverse method that changes the array by inverting the order in which
